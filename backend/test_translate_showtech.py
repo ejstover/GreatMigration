4076ed0176375aebc1fae769622c17da10e00f66
Eric Stover <ericstover2010@gmail.com>
2025-09-12 15:49:33 -0400

    Handle 10GBaseTX media type

---
+++ backend/test_translate_showtech.py
@@ -0,0 +1,28 @@
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
+from backend.translate_showtech import find_copper_10g_ports
+
+
+def test_find_copper_10g_ports():
+    text = (
+        "Switch 1\n"
+        "TenGigabitEthernet1/1/1 is up, line protocol is up\n"
+        "  Hardware is Ten Gigabit Ethernet\n"
+        "  Media Type is 10GBaseT\n"
+        "Te1/1/2 is up\n"
+        "  Media Type is 10GBaseSR\n"
+        "Te1/1/3 is up, line protocol is up\n"
+        "  Full-duplex, 10Gb/s, media type is 100/1000/2.5G/5G/10GBaseTX\n"
+        "Switch 2\n"
+        "Te2/1/1 is up, line protocol is up\n"
+        "  Hardware is Ten Gigabit Ethernet\n"
+        "  media type is 10GBaseT\n"
+    )
+    ports = find_copper_10g_ports(text)
+    assert ports == {
+        "Switch 1": ["TenGigabitEthernet1/1/1", "Te1/1/3"],
+        "Switch 2": ["Te2/1/1"],
+    }
